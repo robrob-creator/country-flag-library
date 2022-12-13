7a4e414ac9205eab8be2fb75958bd5bdc7f00e4e
robert-moonshot <rob.moonshot@gmail.com>
2022-12-13 17:33:58 +0800

    mv-mw

---
+++ src/components/countries/nc.tsx
@@ -0,0 +1,21 @@
+export interface LogoProps {
+  className?: string;
+  width?: string;
+  height?: string;
+}
+
+export const NC = ({ width, height, className }: LogoProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 3 2"
+      width={width}
+      height={height}
+      className={className}
+    >
+      <path fill="#EC1920" d="M0 0h3v2H0z" />
+      <path fill="#fff" d="M0 0h2v2H0z" />
+      <path fill="#051440" d="M0 0h1v2H0z" />
+    </svg>
+  );
+};
